7207d261c88eb52034ada3b9af97484655a2c866
Joseph Crachiolo <crachioloj@gmail.com>
2022-09-11 15:57:05 -0400

    Fix rubocop warnings

--- client/client.rb
+++ client/client.rb
@@ -1,108 +1,115 @@
 require 'rest-client'
 require 'json'
 
+# Client for interacting with 'artworks' and 'artist' APIS
 class Client
+  ARTWORKS_URL = 'http://localhost:4567/artworks'.freeze
+  ARTIST_URL = 'http://localhost:4567/artist'.freeze
 
-  # /artworks endpoint
-  ARTWORKS_URL = "http://localhost:4567/artworks"
-  ARTIST_URL = "http://localhost:4567/artist"
-
-  # Constants
   ARTWORKS_LIMIT = 10
-  PRIMARY_COLORS = ["red", "blue", "yellow"]
+  PRIMARY_COLORS = %w[red blue yellow].freeze
 
-  # There isn't an easy way to get the total number of artworks or remaining pages
-  # from the API, so this is hardcoded for now...
+  # 'Artworks' API doesn't expose a way to get the total items or remaining pages
+  # that match the query, so this is hardcoded for now...
   ARTWORKS_MAX_PAGE = 50
 
   def retrieve(options = {})
     return unless options.respond_to?(:each)
 
-    # Page number should be at least 1
-    currentPage = options[:page].nil? ? 1 : [1, options[:page]].max
+    current_page = get_current_page(options)
 
-    artworks = get_artworks(options, currentPage)
+    artworks = get_artworks(options, current_page)
 
     return unless artworks.respond_to?(:each)
 
-    ids = artworks.map {|a| a["id"]}.uniq
-
-    for_sale = get_artwork_for_sale(artworks)
-
-    sold_primary_count = artworks
-      .select { |a| a["availability"] == "sold" && PRIMARY_COLORS.include?(a["dominant_color"])}
-      .count
-
-    artist_ids = artworks.map {|a| a["artist_id"]}.uniq
-
-    artist_names = artist_ids
-      .map {|id| get_artist(id)}
-      .select {|a| a.has_key?("name")} # ensure each item is valid and has name key
-      .map {|a| a["name"]}
-      .sort
-    
-    response = {}
-
-    response[:ids] = ids
-    response[:for_sale] = for_sale.sort { |a, b| a[:id] <=> b[:id]}
-    response[:soldPrimaryCount] = sold_primary_count
-    response[:artistNames] = artist_names
-    response[:previousPage] = currentPage == 1 ? nil : currentPage - 1
-    response[:nextPage] = artworks.empty? || currentPage >= ARTWORKS_MAX_PAGE ? nil : currentPage + 1
-
-    response
+    build_response(artworks, current_page)
   end
 
   private
 
+  def build_response(artworks, current_page)
+    response = {}
+    response[:ids] = get_artwork_ids(artworks)
+    response[:for_sale] = get_artwork_for_sale(artworks)
+    response[:soldPrimaryCount] = get_sold_primary_count(artworks)
+    response[:artistNames] = get_artist_names(artworks)
+    response[:previousPage] = get_previous_page(current_page)
+    response[:nextPage] = get_next_page(artworks, current_page)
+    response
+  end
+
+  def get_current_page(options)
+    options[:page].nil? ? 1 : [1, options[:page]].max
+  end
+
+  def get_previous_page(current_page)
+    current_page == 1 ? nil : current_page - 1
+  end
+
+  def get_next_page(artworks, current_page)
+    artworks.empty? || current_page >= ARTWORKS_MAX_PAGE ? nil : current_page + 1
+  end
+
+  def get_artwork_ids(artworks)
+    artworks.map { |a| a['id'] }.uniq
+  end
+
+  def get_sold_primary_count(artworks)
+    artworks
+      .select { |a| a['availability'] == 'sold' && PRIMARY_COLORS.include?(a['dominant_color']) }
+      .count
+  end
+
+  def get_artist_names(artworks)
+    artist_ids = artworks.map { |a| a['artist_id'] }.uniq
+
+    artist_ids
+      .map { |id| get_artist(id) }
+      .select { |a| a.key?('name') }
+      .map { |a| a['name'] }
+      .sort
+  end
+
   def get_artwork_for_sale(artworks)
-    # Task description sounded like isPrimary should be on every for_sale item
-    # and set to true or false accordingly,
-    # but the test suite requires isPrimary only being on items where it is set to true
-    # for_sale = artworks
-    #   .select {|a| a["availability"] == "for_sale"}
-    #   .map {|a| a.merge({"isPrimary" => PRIMARY_COLORS.include?(a["dominant_color"])})}
-    #   .map { |a| a.transform_keys { |k| k.to_sym}}
-    
-    for_sale_primary = artworks
-      .select {|a| a["availability"] == "for_sale" && PRIMARY_COLORS.include?(a["dominant_color"])}
-      .map {|a| a.merge({"isPrimary" => true})}
-      .map { |a| a.transform_keys { |k| k.to_sym}}
-
-    for_sale_not_primary = artworks
-      .select {|a| a["availability"] == "for_sale" && !PRIMARY_COLORS.include?(a["dominant_color"])}
-      .map { |a| a.transform_keys { |k| k.to_sym}}
-
+    for_sale_primary = get_for_sale_primary(artworks)
+    for_sale_not_primary = get_for_sale_nonprimary(artworks)
     for_sale = for_sale_primary + for_sale_not_primary
+    for_sale.sort { |a, b| a[:id] <=> b[:id] }
+  end
+
+  def get_for_sale_primary(artworks)
+    artworks
+      .select { |a| a['availability'] == 'for_sale' && PRIMARY_COLORS.include?(a['dominant_color']) }
+      .map { |a| a.merge({ 'isPrimary' => true }) }
+      .map { |a| a.transform_keys(&:to_sym) }
+  end
+
+  def get_for_sale_nonprimary(artworks)
+    artworks
+      .select { |a| a['availability'] == 'for_sale' && !PRIMARY_COLORS.include?(a['dominant_color']) }
+      .map { |a| a.transform_keys(&:to_sym) }
   end
 
   def get_artist(artist_id)
-    begin
-      response = RestClient.get ARTIST_URL, params: {id: artist_id}
-      json = JSON.parse(response)
-      if json.kind_of?(Array)
-        json.first
-      end
-    rescue => error
-      puts error
-    end
+    response = RestClient.get ARTIST_URL, params: { id: artist_id }
+    json = JSON.parse(response)
+    json.first if json.is_a?(Array)
+  rescue StandardError => e
+    puts e
   end
 
-  def get_artworks(options = {}, currentPage)
-    offset = (currentPage - 1) * ARTWORKS_LIMIT
+  def get_artworks(options, current_page)
+    offset = (current_page - 1) * ARTWORKS_LIMIT
 
-    params = {limit: ARTWORKS_LIMIT, offset: offset}
+    params = { limit: ARTWORKS_LIMIT, offset: offset }
 
-    if (options.key?(:dominant_color))
-      params[:dominant_color]= options[:dominant_color]
-    end
+    params[:dominant_color] = options[:dominant_color] if options.key?(:dominant_color)
 
     begin
-      response = RestClient.get ARTWORKS_URL, {params: params}
-      artworks = JSON.parse(response)
-    rescue => error
-      puts error
+      response = RestClient.get ARTWORKS_URL, { params: params }
+      JSON.parse(response)
+    rescue StandardError => e
+      puts e
     end
   end
-
 end
